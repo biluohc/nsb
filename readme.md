7ec9e298ddf5a93a84c3fc11447e1b1b20aaf100
biluohc <biluohc@qq.com>
2017-11-18 19:30:42 +0800

    0.1.0

---
+++ readme.md
@@ -0,0 +1,13 @@
+
+## 暖手宝
+
+### 安装
+```rust
+cargo install --git  https://github.com/biluohc/nsb
+```
+
+### 使用
+`nsb 死循环线程数量`, 默认是 1
+
+### Ps: 是不是要加个循环的频率(插入sleep)?
+
---
+++ src/main.rs
@@ -0,0 +1,62 @@
+
+/*!
+# 暖手宝
+
+## 安装
+```rust
+cargo install --git  https://github.com/biluohc/nsb
+```
+
+## 使用
+`nsb 死循环线程数量`, 默认是 1
+
+## Ps: 是不是要加个循环的频率(插入sleep)?
+
+*/
+extern crate signalbool;
+use signalbool::SignalBool;
+
+use std::thread::{sleep, spawn};
+use std::process::exit;
+use std::time::Duration;
+use std::env::args;
+
+
+fn main() {
+    let help = || {
+        println!("Usage:\n  nsb <Thread_number[Default: 1]>");
+        exit(1);
+    };
+
+    let thread_number = args()
+        .nth(1)
+        .and_then(|s| s.parse::<usize>().map_err(|_| help()).ok())
+        .unwrap_or(1);
+    let sb = SignalBool::new(&[signalbool::Signal::SIGINT], signalbool::Flag::Restart)
+        .map_err(|e| eprintln!("Register Signal failed: {:?}", e))
+        .unwrap();
+        
+    loops(thread_number);
+
+    println!(
+        "nsb running on {} threads\nHit CTRL-C to stop the server",
+        thread_number
+    );
+    loop {
+        sleep(Duration::from_millis(1));
+        if sb.caught() {
+            break;
+        }
+    }
+}
+
+fn loops(number: usize) {
+    // if the loop is empty, process will been kill(Address boundary error), why?
+    (0..number).into_iter().for_each(|_| {
+        let _ = spawn(move || loop {
+            if args().count() > std::usize::MAX {
+                println!("{}", args().count());
+            }
+        });
+    })
+}
