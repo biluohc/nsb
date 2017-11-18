dcfbb96757dbf5a52cf4eebe15ed317606a5663f
biluohc <biluohc@qq.com>
2017-11-18 19:40:37 +0800

    改默认线程数为机器线程数的一半.

--- src/main.rs
+++ src/main.rs
@@ -8,13 +8,14 @@ cargo install --git  https://github.com/biluohc/nsb
 ```
 
 ## 使用
-`nsb 死循环线程数量`, 默认是 1
+`nsb 死循环线程数量`, 默认是机器线程数目的一半.
 
 ## Ps: 是不是要加个循环的频率(插入sleep)?
 
 */
 extern crate signalbool;
 use signalbool::SignalBool;
+extern crate num_cpus;
 
 use std::thread::{sleep, spawn};
 use std::process::exit;
@@ -31,11 +32,11 @@ fn main() {
     let thread_number = args()
         .nth(1)
         .and_then(|s| s.parse::<usize>().map_err(|_| help()).ok())
-        .unwrap_or(1);
+        .unwrap_or_else(default_number);
     let sb = SignalBool::new(&[signalbool::Signal::SIGINT], signalbool::Flag::Restart)
         .map_err(|e| eprintln!("Register Signal failed: {:?}", e))
         .unwrap();
-        
+
     loops(thread_number);
 
     println!(
@@ -59,4 +60,13 @@ fn loops(number: usize) {
             }
         });
     })
+}
+
+fn default_number() -> usize {
+    let cpus = num_cpus::get();
+    if cpus < 2 {
+        1
+    } else {
+        cpus / 2
+    }
 }
